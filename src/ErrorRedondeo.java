a8dc0b3f8e149341642fe56456f38513f80a012d
camo <Camotubi>
2017-04-13 22:42:55 -0500

    fix ErrorRedondeo related functions

--- src/ErrorRedondeo.java
+++ src/ErrorRedondeo.java
@@ -2,16 +2,28 @@
 public class ErrorRedondeo {
 	static float sumNumSimple(int times,float num)
 	{
+		float result=0;
 		for(int i=0;i<times;i++)
-			num=num+num;
-		return num;
+		{
+			result=result+1;
+			if(num<Float.MAX_VALUE)
+				System.out.println(result);
+		
+		}
+		return result;
 	}
 	
 	static double sumNumDouble(int times,double num)
 	{
+		double result=0;
 		for(int i=0;i<times;i++)
-			num=num+num;
-		return num;
+		{
+			result=result+1;
+			if(num<Float.MAX_VALUE)
+				System.out.println(result);
+		
+		}
+		return result;
 	}
 	
 	
